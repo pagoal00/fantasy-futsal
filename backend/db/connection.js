1ed98fc35f3b02945dbacdf476bd0bda06a56b8f
Pablo Gonzalez <pablo@fantasyfutsal.com>
2025-06-02 09:46:31 +0200

    Adaptado a ES Modules, añadido .env y .gitignore

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,10 +1,12 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: "mysql-fantasyfutsal.alwaysdata.net",
-  user: "416217",
-  password: "FantasyFutsal_Pablo00",
-  database: "fantasyfutsal_futsal",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
 });
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,19 @@
 import express from "express";
 import path from "path";
-import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
-import cron from "./cronJobs.js";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import dotenv from "dotenv";
+import "./cronJobs.js";
+
 
 dotenv.config();
 
-// Necesario para obtener __dirname en ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "frontend")));
 
 app.get("/logout", (req, res) => {
-  req.session?.destroy(() => {
+  req.session.destroy(() => {
     res.redirect("/index.html");
   });
 });
@@ -27,5 +23,5 @@ app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
